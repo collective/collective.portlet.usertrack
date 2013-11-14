236fc084e682db00fa5ce71845dc26f2f2cf7eef
hvelarde <hector.velarde@gmail.com>
2013-11-14 15:59:26 -0200

    Fix package distribution
    * Rename README and changelog files to indicate reStructuredText
    * Fix package classifiers
    * Simplify MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,26 +1,36 @@
-from setuptools import setup, find_packages
-import os
+# -*- coding:utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
 
 version = '1.1.2.dev0'
+long_description = (
+    open('README.rst').read() + '\n' +
+    open('CHANGES.rst').read()
+)
 
 setup(name='collective.portlet.usertrack',
       version=version,
       description="User activity portlet",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=long_description,
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Framework :: Plone :: 4.1',
+          'Framework :: Plone :: 4.2',
+          'Framework :: Plone :: 4.3',
+          'Framework :: Plone',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
       keywords='user tracking plone',
       author='Wichert Akkerman',
       author_email='wichert@wiggy.net',
       url='http://pypi.python.org/pypi/collective.portlet.usertrack',
-      license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.portlet'],
       include_package_data=True,
